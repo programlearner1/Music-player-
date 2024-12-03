8c815f51d131fb27613d9e1dafd5fb26965ad392
Programlearner1 <113779810+programlearner1@users.noreply.github.com>
2024-12-03 15:20:41 +0530

    image changed

--- script.js
+++ script.js
@@ -17,12 +17,13 @@ document.addEventListener('DOMContentLoaded', () => {
     const songs = [
         { title: "Bujji thalli", artist:"Javed Ali and Devi Sri Prasad", url: "music.mp3", image: "img1.jpeg" },
         { title: "Aa bandham abadhama", artist:"Vaishnavi Kovvuri", url: "song2.mp3", image: "thumbnail.jpg" },
-        { title: "Hey rangule", artist:" Anurag Kulkarni", url: "song3.mp3", image: "Rangule.png" },
+        { title: "Hey rangule", artist:" Anurag Kulkarni", url: "song3.mp3", image: "ranguleimg.jpg" },
         { title: "Rayani kadhale", artist:" M.S Krsna and Meha Agarwal", url: "song4.mp3", image: "download.jpeg" },
         { title: "Manasilaayo", artist:"Anirudh Ravichander", url: "song5.mp3", image: "1.jpg" },
     ];
 
     let currentSongIndex = 0;
+    changeSong(currentSongIndex);
 
     // Join a room
     joinRoomBtn.addEventListener('click', () => {
